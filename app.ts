e14a73f3432e7a8bae505203bc88513cd86bec94
Felipe Coury <felipe.coury@gmail.com>
2015-05-22 10:14:00 -0300

    Added interaction

--- app.ts
+++ app.ts
@@ -12,23 +12,26 @@ bootstrap,
 })
 @View({
   template: `
-        <section class="new-link">
-          <div class="control-group">
-            <div><label for="title">Title:</label></div>
-            <div><input name="title"></div>
-          </div>
-          <div class="control-group">
-            <div><label for="link">Link:</label></div>
-            <div><input name="link"></div>
-          </div>
+    <section class="new-link">
+      <div class="control-group">
+        <div><label for="title">Title:</label></div>
+        <div><input name="title" #newtitle></div>
+      </div>
+      <div class="control-group">
+        <div><label for="link">Link:</label></div>
+        <div><input name="link" #newlink></div>
+      </div>
 
-          <button>Submit Link</button>
-        </section>
-      `,
+      <button (click)="addArticle(newtitle, newlink)">Submit Link</button>
+    </section>
+  `,
 
   directives: [For]
 })
 class RedditApp {
+  addArticle(title, link) {
+    console.log("Adding article with title", title.value, "and link", link.value);
+  }
 }
 
 bootstrap(RedditApp);

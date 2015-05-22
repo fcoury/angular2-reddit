288497634688ebe49150cadbb0e6812e0057f9f6
Felipe Coury <felipe.coury@gmail.com>
2015-05-22 10:04:23 -0300

    Added Reddit form

--- app.ts
+++ app.ts
@@ -1,30 +1,34 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
 
 import {
-  Component,
-  For,
-  View,
-  bootstrap,
+Component,
+For,
+View,
+bootstrap,
 } from "angular2/angular2";
 
 @Component({
-  selector: 'hello-world'
+  selector: 'reddit'
 })
 @View({
   template: `
-  <ul>
-    <li *for="#name of names">Hello {{ name }}</li>
-  </ul>
-  `,
+        <section class="new-link">
+          <div class="control-group">
+            <div><label for="title">Title:</label></div>
+            <div><input name="title"></div>
+          </div>
+          <div class="control-group">
+            <div><label for="link">Link:</label></div>
+            <div><input name="link"></div>
+          </div>
+
+          <button>Submit Link</button>
+        </section>
+      `,
 
   directives: [For]
 })
-class HelloWorld {
-  names: Array<string>;
-
-  constructor() {
-    this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
-  }
+class RedditApp {
 }
 
-bootstrap(HelloWorld);
+bootstrap(RedditApp);
